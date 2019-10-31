0e1a8ee0195a1ad44f58d1e61e12d8d6b75d3df3
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2019-10-31 11:52:57 -0400

    class notes on CART

--- regression_trees.R
+++ regression_trees.R
@@ -1,12 +1,68 @@
+
+
+# load packages and data --------------------------------------------------
+
+
 library(tidyverse)
 library(dslabs)
 data("olive")
 
+
+# look at the data --------------------------------------------------------
+
+
 head(olive)
+
 
 olive %>% 
   select(-area) %>% 
-  gather(fatty_acid, percentage, -region) %>% 
+  gather(key = fatty_acid, value = percentage, -region) %>% glimpse#View
+
+# third argument can be any kind of "select()" syntax: e.g. -region
+
+
+olive %>% 
+  select(-area) %>% glimpse %>% 
+  gather(key = fatty_acid, value = percentage, 2:9) %>% glimpse
+
+olive %>% 
+  select(-area) %>% glimpse %>% 
+  gather(key = fatty_acid, value = percentage, palmitic:eicosenoic) %>% glimpse
+
+## gather -- and MAKE A UNIQUE ROW NUMBER
+olive_long <- olive %>% 
+  select(-area) %>% #glimpse %>% 
+  mutate(rownum = seq_along(region)) %>% 
+  gather(key = fatty_acid, value = percentage, ends_with("ic")) %>% glimpse
+
+
+olive_no_area <- olive %>% 
+  select(-area, -region)
+
+olive_no_area %>% #glimpse %>% 
+  gather(key = fatty_acid, value = percentage) %>% glimpse
+
+# the opposite of gather is spread()
+olive_long %>% head
+olive_long %>% 
+  arrange(rownum) %>% head(10)
+
+## reverse a gather()
+
+olive_long %>% 
+  spread(fatty_acid, percentage) %>% glimpse
+# doesn't work!! 
+
+## the new, modern syntax -- pivot_longer
+
+olive_long <- olive %>% 
+  select(-area) %>% 
+  mutate(rownum = seq_along(region)) %>% 
+  pivot_longer(cols = ends_with("ic"), names_to = "fatty_acid", values_to = "percentage")
+
+browseVignettes()
+
+olive_long %>% 
   ggplot(aes(x = region, y = percentage, fill = region)) + 
   # geom_point() + 
   facet_wrap(~fatty_acid, scale = "free") + 
@@ -24,11 +80,23 @@ plot(fit)
 
 library(rpart)
 
+set.seed(4812)
+kill_these <- sample(1:572, 5, replace = FALSE)
+olive$arachidic[kill_these] <- NA_real_
 
-olive_fit_full <- rpart(region ~ . , data = select(olive, - area))
+
+olive_ruined <- olive
+set.seed(4812)
+kill_these <- sample(1:572, 80, replace = FALSE)
+olive_ruined$linoleic[kill_these] <- NA_real_
+
+olive$arachidic
+olive_fit_full <- rpart(region ~ . , data = select(olive_ruined, - area))
 
 plot(olive_fit_full)
 text(olive_fit_full)
+
+olive_fit_full
 
 data("polls_2008")
 
@@ -43,8 +111,11 @@ poll_model <- rpart(margin ~ day, data = polls_2008)
 plot(poll_model, margin = 0.1)
 text(poll_model)
 
+# predict with no new data == prediction on original data
+
 polls_2008 %>% 
   mutate(y_predicted = predict(poll_model)) %>% 
   ggplot(aes(x = day, y = margin)) + 
   geom_point() + 
   geom_step(aes(y = y_predicted), col = "darkgreen")
+
